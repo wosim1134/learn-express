7c0533f1aba1b003a8dcfeb9894b61825190e4fb
wosim1134 <pubglnar@gmail.com>
2024-04-04 10:40:26 +0900

    multer를 이용한 파일 업로드

--- app.js
+++ app.js
@@ -1,19 +1,106 @@
-//
-const express = require('express');
+const express = require('express'); //
+const morgan = require('morgan'); //
+const cookieParser =require('cookie-parser'); //
+const session = require('express-session'); //
+const dotenv = require('dotenv'); //
+const path = require('path'); //
+
+dotenv.config(); //
+
+const app = express(); //
+
+app.set('port', process.env.PORT || 3000); //
+    //
+
+app.use(morgan('dev')); //
 
 //
-const path = require('path');
+//
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 //
-const app = express();
-
-//
-app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+    //
+app.use(express.urlencoded({ extended: false }));
+    //
     //
 
 //
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+    //
+    //
+
+//
+//
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}))
+    //
+    //
+    //
+    //
+        //
+        //
+    //
+
+const multer = require('multer'); //
+const fs = require('fs'); //
+
+//
+try {
+    //
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    //
+    fs.mkdirSync('uploads');
+}
+
+//
+const upload = multer({
+    //
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/'); //
+        },
+        filename(req, file, done) {
+            //
+            const ext = path.extname(file.originalname);
+            //
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    //
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+//
+app.get('/upload', (rea, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+//
+app.post('/upload', upload.single('image'), (req, res) => {
+    //
+    //
+    console.log(req.file);
+    //
+    res.send('ok');
+});
+
+//
+//
+app.get('/', (req, res, next) => {
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
 });
     //
     //
@@ -22,25 +109,18 @@ app.get('/', (req, res) => {
     //
     //
 
+//
+//
+//
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+})
+    //
+    //
 
 //
 //
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
